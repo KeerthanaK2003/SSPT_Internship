22dc607026ed18a72ae1b0d1255a369383fb562d
Keerthana K <keerthanak.ranganath@gmail.com>
2025-09-02 22:19:40 +0530

    Add files via upload

--- top.v
+++ top.v
@@ -3,25 +3,24 @@ module top (
     output [9:0] LEDR    // LEDs on DE10-Lite
 );
 
-wire sum, cout;
-wire a, b, cin;
+wire sum, carry;
+wire a, b;
 
-assign a   = SW[0];  // Input 'a' from SW[0]
-assign b   = SW[1];  // Input 'b' from SW[1]
-assign cin = SW[2];  // Carry-in from SW[2]
+assign a = SW[0];  // Input 'a' from SW[0]
+assign b = SW[1];  // Input 'b' from SW[1]
 
-// Instantiate the Full Adder
-fa uut (
+// Instantiate the Half Adder
+ha uut (
     .a(a),
     .b(b),
-    .cin(cin),
     .sum(sum),
-    .cout(cout)
+    .carry(carry)
 );
 
 // Display sum and carry on LEDs
 assign LEDR[0] = sum;     // LEDR[0] = sum output
-assign LEDR[1] = cout;   // LEDR[1] = carry output
+assign LEDR[1] = carry;   // LEDR[1] = carry output
 assign LEDR[9:2] = 8'b0;  // Remaining LEDs OFF
 
 endmodule
+
